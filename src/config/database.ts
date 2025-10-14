c8814d5a0334f85dff112fe61013191d3311672d
julian2825 <judasugi.28.25.7@gmail.com>
2025-10-14 13:43:21 -0500

    integracion de lo cristina

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,12 +3,13 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',           // Cambia si usas otro usuario
-  password: 'bura12325',           // Pon tu contraseña de MySQL aquí
-  database: 'banca_uno',
-  port: 3307,
+  password: 'julian28257',           // Pon tu contraseña de MySQL aquí
+  database: 'banca_uno', // Asegúrate de que esta base de datos exista
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 export default pool;
+
